29c10f6d67b3be90d733ce4e3758d315298faee2
monil2043 <monil.ranka@gmail.com>
2023-02-03 13:00:15 +0530

    updating destroy configs

--- instance.tf
+++ instance.tf
@@ -1,3 +1,12 @@
 provider "aws" {
 	region = "ap-south-1"
 }
+
+
+terraform {
+  backend "s3" {
+    bucket = "save-application-jars"
+    key    = "key/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
